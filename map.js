34704f071e4fbfd20be041e3114f86328fc93bd9
Bruna Savelli <bnsavelli@gmail.com>
2024-03-17 11:37:31 -0300

    Exemplo map

---
+++ map.js
@@ -0,0 +1,5 @@
+//O método map cria um novo array com os resultados da chamada de uma função para cada elemento do array, em baixo multipliquei por 2
+
+let array = [2, 4, 6];
+let newArray = array.map(element => element * 2);
+console.log(newArray);  //saida: 4, 8 ,12
