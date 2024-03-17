7e3365920e0b91460397ea3ea77b9935ea01f3db
Bruna Savelli <bnsavelli@gmail.com>
2024-03-17 11:36:30 -0300

    Exemplo join

---
+++ join.js
@@ -0,0 +1,5 @@
+//O método join une todos os elementos de um array em uma string.
+
+let array = ['Nickolas', 'me', "ama"];
+let joinedString = array.join(' ');
+console.log(joinedString); // Saída: "Nickolas me ama"
