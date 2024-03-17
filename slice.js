3e094bf6d3a7d9b9b21f56f061dc75fb46a2d274
Bruna Savelli <bnsavelli@gmail.com>
2024-03-17 11:39:30 -0300

    Exemplo slice

---
+++ slice.js
@@ -0,0 +1,5 @@
+//O método slice retorna uma cópia de uma parte de um array, dentro de um novo array
+
+let array = [1, 4, 3, 8, 5];
+let newArray = array.slice(1, 3); // Retorna elementos do índice 1 ao índice 2 
+console.log(newArray); //saida: 4 , 3
