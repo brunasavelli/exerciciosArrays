ec482fa8b57dd38170e684c390fd562f88b662fd
Bruna Savelli <bnsavelli@gmail.com>
2024-03-17 11:35:15 -0300

    Exemplo forEach

---
+++ forEach.js
@@ -0,0 +1,5 @@
+//O método forEach executa uma função fornecida uma vez para cada elemento do array.
+
+let array = ["maça", "banana", "uva"];
+array.forEach(element => console.log(element)); 
+//saida: "maça", "banana", "uva"
