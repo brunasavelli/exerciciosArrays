79c846be06ad4b6832ab653f9eb5f6d11ad094a4
Bruna Savelli <bnsavelli@gmail.com>
2024-03-17 11:27:28 -0300

    First commit deu certo

---
+++ concat.js
@@ -0,0 +1,6 @@
+//O método concat retorna um novo array resultante da concatenação de outros arrays.
+
+let array1 = ["Nickolas", "Lira",];
+let array2 = ["Bruna", "Savelli"];
+let novoArray = array1.concat(array2);
+console.log(novoArray); //saida: "Nickolas" , "Lira" , "Bruna" , "Savelli";
