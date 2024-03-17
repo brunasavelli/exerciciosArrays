c740022f56e863b5c47960dc50a26a8c49324214
Bruna Savelli <bnsavelli@gmail.com>
2024-03-17 11:33:39 -0300

    Exemplo find

---
+++ find.js
@@ -0,0 +1,5 @@
+//O método find retorna o primeiro elemento do array que satisfaz a função de teste fornecida.
+
+let array = [1, 2, 3, 4, 5];
+let foundElement = array.find(element => element > 2);
+console.log(foundElement); //saida: 3
